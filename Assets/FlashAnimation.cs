ae7601320c9e68dfdd0bf796e99e069774f215aa
Leobizaz <leobizaz12@gmail.com>
2020-04-15 22:26:05 -0300

    rush de prototipo adiado

---
+++ Assets/FlashAnimation.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FlashAnimation : MonoBehaviour
+{
+    public void Play()
+    {
+        transform.parent.transform.position = new Vector3(0.29f, -90.655f, 0);
+        transform.parent.transform.rotation = Quaternion.Euler(0, 0, 0);
+        transform.parent.transform.localScale = new Vector3(-1f, 1, 1);
+    }
+}
--- Assets/Scripts/DownPlatform.cs
+++ Assets/Scripts/DownPlatform.cs
@@ -6,19 +6,32 @@ public class DownPlatform : MonoBehaviour
 {
     bool playerOnTop;
     private PlatformEffector2D effector;
-    public float wait;
+    public float wait = 0.3f;
     float normalOffset;
-
-
+    [SerializeField] Collider2D[] neighbors;
+    BoxCollider2D boxcollider;
+    ContactFilter2D contactFilter = new ContactFilter2D();
     void Start()
     {
+        contactFilter.layerMask = 8;
+        boxcollider = GetComponent<BoxCollider2D>();
+        neighbors = new Collider2D[2];
         effector = GetComponent<PlatformEffector2D>();
         normalOffset = effector.rotationalOffset;
+        boxcollider.OverlapCollider(contactFilter, neighbors);
     }
 
     private void OnCollisionEnter2D(Collision2D collision)
     {
         if(collision.gameObject.tag == "Player")
+        {
+            playerOnTop = true;
+        }
+    }
+
+    private void OnCollisionStay2D(Collision2D collision)
+    {
+        if (collision.gameObject.tag == "Player")
         {
             playerOnTop = true;
         }
@@ -42,21 +55,65 @@ public class DownPlatform : MonoBehaviour
         if (Input.GetAxis("Vertical") <= -0.5f)
         {
             wait = 0;
-            if (wait <= 0.5 && Input.GetKey(KeyCode.Space) && playerOnTop)
+            if (wait <= 0.3 && Input.GetKey(KeyCode.Space) && playerOnTop)
             {
-                effector.rotationalOffset = normalOffset - 180; ;
-              
+                Twist();
+                TwistNeighbors(null);   
             }
         }
-        if (wait == 0.5f)
+        if (wait == 0.3f)
         {
             StopAllCoroutines();
-            effector.rotationalOffset = normalOffset;
+            UnTwist();
+            UnTwistNeighbors(null);
         }
     }
     private IEnumerator Wait()
     {
         yield return new WaitForSeconds(0.3f);
-        wait = 0.5f;
+        wait = 0.3f;
+    }
+
+    void TwistNeighbors(GameObject exclude)
+    {
+        foreach(Collider2D collider in neighbors)
+        {
+            if (collider.gameObject.GetComponent<DownPlatform>())
+            {
+                collider.gameObject.GetComponent<DownPlatform>().Twist();
+                //if (collider.gameObject != exclude) collider.gameObject.GetComponent<DownPlatform>().TwistNeighbors(this.gameObject);
+            }
+        }
+    }
+
+    void UnTwistNeighbors(GameObject exclude)
+    {
+        if (neighbors.Length > 0)
+        {
+            foreach (Collider2D collider in neighbors)
+            {
+                if (collider != null)
+                {
+                    if (collider.gameObject.GetComponent<DownPlatform>() != null)
+                    {
+
+                        collider.gameObject.GetComponent<DownPlatform>().UnTwist();
+                        //if (collider.gameObject != exclude) collider.gameObject.GetComponent<DownPlatform>().UnTwistNeighbors(this.gameObject);
+                    }
+                }
+            }
+        }
+    }
+
+    public void Twist()
+    {
+        effector.rotationalOffset = normalOffset - 180;
+        boxcollider.enabled = false;
+    }
+
+    public void UnTwist()
+    {
+        effector.rotationalOffset = normalOffset;
+        boxcollider.enabled = true;
     }
 }
