b6f7ce197ebb7738e58a138dda78d886948e6922
GuiSR388 <guicrilopez@users.noreply.github.com>
2020-04-20 18:01:10 -0300

    Menu Adicionado, Pause (bem basicão) adicionado e display do dano atualizado

---
+++ Assets/MenuController.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class MenuController : MonoBehaviour
+{
+    public AudioSource Begin;
+    public Animator anim;
+
+    public void PlayAudio()
+    {
+        Begin.Play();
+        anim.Play("Menu Out");
+        Invoke("LoadGame", 5f);
+    }
+    public void LoadGame()
+    {
+        SceneManager.LoadScene("CENAPRINCIPAL");
+    }
+}
