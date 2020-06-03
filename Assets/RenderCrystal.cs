27edbd73dbe1a3fc82a6e5c55e75b814566dbd9b
Leobizaz <leobizaz12@gmail.com>
2020-06-03 06:07:34 -0300

    Shader de água e cristais

---
+++ Assets/RenderCrystal.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RenderCrystal : MonoBehaviour
+{
+    private Material mat;
+    private SpriteRenderer rend;
+    public Camera cam;
+    RenderTexture renderTex;
+
+    [ExecuteInEditMode]
+
+    private void Start()
+    {
+        rend = GetComponent<SpriteRenderer>();
+        mat = rend.material;
+        renderTex = new RenderTexture(500, 500, 0, RenderTextureFormat.ARGB32);
+        renderTex.wrapMode = TextureWrapMode.Repeat;
+        renderTex.Create();
+        cam.targetTexture = renderTex;
+
+        mat.SetTexture("_RenderTex", renderTex);
+
+    }
+
+
+}
