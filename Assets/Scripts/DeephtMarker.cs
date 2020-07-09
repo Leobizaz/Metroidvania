c97fd5c8232d57356b24c8c98b2e5e584708b87d
GuiSR388 <guicrilopez@users.noreply.github.com>
2020-07-09 11:47:05 -0300

    Medidor de profundidade pog

---
+++ Assets/Scripts/DeephtMarker.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+public class DeephtMarker : MonoBehaviour
+{
+
+    public GameObject Player;
+    public GameObject Marker;
+    public TextMeshProUGUI Valor;
+    private float Dist;
+    private float PrintDist;
+
+   
+    void Update()
+    {
+        Marker.transform.position = new Vector3(Player.transform.position.x, Marker.transform.position.y, Marker.transform.position.z);
+        Dist = Vector2.Distance(Player.transform.position, Marker.transform.position);
+        PrintDist = Dist / 4;
+        Valor.text = " " + (Mathf.Round(PrintDist)) + "m";
+    }
+}
