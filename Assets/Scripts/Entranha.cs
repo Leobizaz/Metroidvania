9d86f168368f1689922e2ede3f50fd3e477edb2d
Leonardo Biz Villar <leobizaz12@gmail.com>
2020-09-16 22:33:26 -0300

    Level design expandido, charger finalizado

--- Assets/Scripts/Entranha.cs
+++ Assets/Scripts/Entranha.cs
@@ -29,7 +29,8 @@ public class Entranha : MonoBehaviour
             }
             else
             {
-                GetHit();
+                if(!cortado)
+                    GetHit();
             }
         }
     }
