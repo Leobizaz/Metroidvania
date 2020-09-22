d28d0547b14bd74f2bce518af4fb30c38e31207d
Leonardo Biz Villar <leobizaz12@gmail.com>
2020-09-22 05:58:40 -0300

    kkkk tubo

---
+++ Assets/CameraTubo.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Cinemachine;
+using BansheeGz;
+using BansheeGz.BGSpline.Curve;
+using BansheeGz.BGSpline.Components;
+
+public class CameraTubo : MonoBehaviour
+{
+    public CinemachineDollyCart cart;
+    bool moving;
+    public BGCurve curva;
+    public BGCcMath spline;
+    float distance;
+    public GameObject cameraPLAYER;
+
+    private void Start()
+    {
+
+    }
+
+    private void Update()
+    {
+        if (moving)
+        {
+            distance += 14 * Time.deltaTime;
+
+            Vector3 tangent;
+            cart.gameObject.transform.position = spline.CalcPositionAndTangentByDistance(distance, out tangent);
+            //cart.gameObject.transform.rotation = Quaternion.AngleAxis(Mathf.Atan2(-tangent.y, -tangent.x) * Mathf.Rad2Deg, Vector3.forward);
+
+            if (distance >=255)
+            {
+                moving = false;
+                cameraPLAYER.SetActive(true);
+                //cart.m_Speed = 0;
+                gameObject.SetActive(false);
+            }
+        }
+    }
+
+    private void OnEnable()
+    {
+        cart.gameObject.transform.position = curva.Points[0].PositionWorld;
+        distance = 0;
+        //cart.m_Position = 0;
+        moving = true;
+        //cart.m_Speed = 2;
+
+    }
+}
---
+++ Assets/InteractTubo.cs
@@ -0,0 +1,67 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InteractTubo : MonoBehaviour
+{
+    public GameObject indicator;
+    bool onArea;
+    public bool oneTime = true;
+    public bool destroy = true;
+    bool once;
+
+    public GameObject cameraPLAYER;
+
+    public GameObject camera;
+    public GameObject player;
+    public GameObject endposition;
+
+    void Start()
+    {
+        indicator.SetActive(false);
+    }
+    void Update()
+    {
+        if (onArea && Input.GetKeyDown(KeyCode.E) && !once)
+        {
+            if (oneTime) once = true;
+            Execute();
+            indicator.SetActive(false);
+            if (destroy)
+            {
+                Destroy(gameObject, 0.1f);
+                Destroy(indicator, 0.1f);
+            }
+        }
+    }
+
+    public void Execute()
+    {
+        camera.SetActive(true);
+        cameraPLAYER.SetActive(false);
+        Invoke("Teleport", 2);
+    }
+
+    public void Teleport()
+    {
+        player.transform.position = endposition.transform.position;
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.tag == "Player")
+        {
+            onArea = true;
+            indicator.SetActive(true);
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        if (collision.tag == "Player")
+        {
+            indicator.SetActive(false);
+            onArea = false;
+        }
+    }
+}
